937a9c286112d89229665f2800e7533369629291
Code Sandbox <hello@codesandbox.io>
2021-10-15 06:03:02 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.eae4f5624fd232fa5886a08e095e993f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a51b302f8bd8735197bb37a17bb1ed1",
+    "url": "https://paulolope.github.io/csb-7f7er/index.html"
+  },
+  {
+    "revision": "fc9214766ca157d129b0",
+    "url": "https://paulolope.github.io/csb-7f7er/static/js/2.e1bded2c.chunk.js"
+  },
+  {
+    "revision": "e9140cc11554cc6d5963",
+    "url": "https://paulolope.github.io/csb-7f7er/static/js/main.6205383d.chunk.js"
+  },
+  {
+    "revision": "e51758e6ad1dae7479e3",
+    "url": "https://paulolope.github.io/csb-7f7er/static/js/runtime~main.bccb2b80.js"
+  }
+]);
